a7850e48ca053b2f3ba075c06eef70f7ff0d56ce
Alisha Mohanty <alishamohanty1416@gmail.com>
2018-08-26 18:17:08 +0530

    removing bugs

--- public/js/viewer.js
+++ public/js/viewer.js
@@ -17,16 +17,4 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
 })
-}
-// function initMap() {
-//     navigator.geolocation.getCurrentPosition(pos => {
-//         const {latitude: lat, longitude: lng} = pos.coords
-//         map = new google.maps.Map(document.getElementById('map'), {
-//             center: {lat, lng},
-//             zoom: 5
-//           })
-//         console.log({lat, lng})
-//     },err => {
-//         console.log(err)
-//     })  
-// }
+})
