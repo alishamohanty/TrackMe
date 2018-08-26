c66cd816339b757e6bec10eb74f4893df34516fb
Alisha Mohanty <alishamohanty1416@gmail.com>
2018-08-26 18:15:49 +0530

    deleting oldmarker

--- public/js/tracker.js
+++ public/js/tracker.js
@@ -1,14 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     const socket = io('/')
+
     const options = {
         enableHighAccuracy: true,
         maximumAge: 0
     }
+
+    socket.emit('registerTracker')
+
     setInterval(() => {
         console.log("Inside setInterval")
         navigator.geolocation.getCurrentPosition(pos => {
             const {latitude: lat, longitude: lng} = pos.coords
-            socket.emit('updateLocation', {lat, lng})
+            socket.emit('registerTracker', {lat, lng})
             console.log({lat, lng})
         },err => {
             console.log(err)
--- public/js/viewer.js
+++ public/js/viewer.js
@@ -1,16 +1,32 @@
+let map
+let markers = new Map()
 document.addEventListener('DOMContentLoaded', () => {
     const socket = io('/')
-    let map
+    socket.on('locationsUpdate', locations => {
+        console.log(locations)
+        locations.forEach(([id, position]) => {
+            const marker = new google.maps.Marker({
+                position,
+                map,
+                title: id
+            })
+        if (markers.has(id)) {
+            const oldMarker = markers.get(id)
+            oldMarker.setMap(null);
+            markers.delete(id); 
+        }
+    })
 })
-function initMap() {
-    navigator.geolocation.getCurrentPosition(pos => {
-        const {latitude: lat, longitude: lng} = pos.coords
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat, lng},
-            zoom: 5
-          })
-        console.log({lat, lng})
-    },err => {
-        console.log(err)
-    })  
 }
+// function initMap() {
+//     navigator.geolocation.getCurrentPosition(pos => {
+//         const {latitude: lat, longitude: lng} = pos.coords
+//         map = new google.maps.Map(document.getElementById('map'), {
+//             center: {lat, lng},
+//             zoom: 5
+//           })
+//         console.log({lat, lng})
+//     },err => {
+//         console.log(err)
+//     })  
+// }
